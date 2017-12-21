f4dd0f9e9b976c6d6f96ecd66323cd78b648d45e
Daniel Rojkind <daniel@dpc.io>
2017-12-22 11:06:07 +1300

    outputs data to a json array

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react';
 import './App.css';
 import logFile from './bluetooth.json';
-import convertHex from 'convert-hex';
+import { LineChart, Line, XAxis, YAxis } from 'recharts';
+import moment from 'moment';
+let blue = [];
+
 
 class App extends Component {
 
@@ -9,37 +12,54 @@ class App extends Component {
     super(props);
     this.state = {
       time: [],
-      dataId: []
+      dataId: [],
+      bluetoothData: []
     }
   }
 
   componentWillMount() {
     console.log('this is working...')
-    logFile.map(files => this.getPressureTemp(files._source.layers.bthci_evt['btcommon.eir_ad.advertising_data']['btcommon.eir_ad.entry']['btcommon.eir_ad.entry.service_data']));
-    console.log(logFile.map(files => files._source.layers.frame['frame.time']));
-    console.log(logFile.map(files => {
-      time : files._source.layers.frame['frame.time']
-    }));
-    console.log(logFile[1]._source.layers.bthci_evt['btcommon.eir_ad.advertising_data']['btcommon.eir_ad.entry']['btcommon.eir_ad.entry.service_data'])
-    console.log(logFile[0]._source.layers.frame['frame.time'])
-
-    this.getPressureTemp('f0:3f:07:88:15:00:c7:01:2f:0a:49');
+    logFile.map(files => this.getPressureTemp(files._source.layers.bthci_evt['btcommon.eir_ad.advertising_data']['btcommon.eir_ad.entry']['btcommon.eir_ad.entry.service_data'], files._source.layers.frame['frame.time']));
   }
 
-  getPressureTemp(data) {
+  componentDidMount() {
+    console.log(JSON.stringify(blue))
+    console.log(blue);
+    console.log(moment('14-05-1991').format('DD-MM-YYYY'));
+  }
+
+  getPressureTemp(data, time) {
     if (data === undefined) {
       console.log('--- this data is undefined ---')
     } else {
       let splitData = data.split(':');
+      // console.log('Device ID: ', splitData[0] + splitData[1])
+      // console.log('Device ID: ', parseInt(splitData[1] + splitData[0], 16))
+      // console.log('Temperature: ', parseInt(splitData[10], 16) - 50)
+      // console.log('Pressure: ', parseInt(splitData[7] + splitData[6], 16));
+      // console.log('Battery: ', parseInt(splitData[9] + splitData[8], 16));
+      // console.log('Time: ', time);
+      blue.push({
+        deviceId: splitData[0] + splitData[1],
+        temperature: parseInt(splitData[10], 16) - 50,
+        pressure: parseInt(splitData[7] + splitData[6], 16),
+        battery: parseInt(splitData[9] + splitData[8], 16),
+        time: time
+      });
 
-      console.log('Temperature: ', convertHex.hexToBytes(splitData[10]).join(',') - 50);
-      console.log('Battery: ', convertHex.hexToBytes(splitData[8] + splitData[9]).join(','));
-      console.log('Pressure: ', convertHex.hexToBytes(splitData[6] + splitData[7]).join(','));
+      //  this.setState({ bluetoothData: [...this.state.bluetoothData, splitData[0] + splitData[1]]})
     }
-
   }
+
   render() {
-    return (<div className="App"></div>);
+    return (
+      <div className="App">
+      <LineChart width={800} height={800} data={blue}>
+        <Line type="monotone" dataKey="uv" stroke="#8884d8"/>
+        <XAxis dataKey="Temperature" />
+        <YAxis dataKey="deviceId" />
+      </LineChart>
+    </div>);
   }
 }
 
